f7614655af961e7fb2782dea29b35948c202ae62
elsh <elshanskiy.p@mail.ru>
2023-04-19 09:39:23 +0300

    Добавлена авторизация, регистрация, аутентификация с помощью Spring Security

--- src/main/java/com/example/finalproject/repositories/CategoryRepository.java
+++ src/main/java/com/example/finalproject/repositories/CategoryRepository.java
@@ -1,9 +1,10 @@
 package com.example.finalproject.repositories;
 
+import com.example.finalproject.models.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface CategoryRepository extends JpaRepository<CategoryRepository, Integer> {
+public interface CategoryRepository extends JpaRepository<Category, Integer> {
     com.example.finalproject.models.Category findByName(String name);
 }
